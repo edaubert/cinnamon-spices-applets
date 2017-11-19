f882ab474cf3392804fc26f7a6fc1ffbbe245a8a
Erwan Daubert <erwan.daubert@gmail.com>
2017-11-19 01:05:32 +0100

    clean code

--- MultiMonitorWindowPositionHandling@edaubert/files/MultiMonitorWindowPositionHandling@edaubert/windows-configuration-manager.sh
+++ MultiMonitorWindowPositionHandling@edaubert/files/MultiMonitorWindowPositionHandling@edaubert/windows-configuration-manager.sh
@@ -7,7 +7,7 @@ function getNbMonitor () {
 
 function getConfigurationName () {
 	monitors=`xrandr | grep connected | grep -v disconnected | cut -d' ' -f1`
-	configurationName=`echo -n ".${monitors}.hotplug"`
+	configurationName=`echo -n ".${monitors}.monitors"`
 	configurationName=`echo -n $configurationName | sed -e "s/ /_/g"`
 	echo $configurationName
 }
@@ -30,7 +30,6 @@ function saveConfiguration () {
 		maximizedVertically=`echo $?`
 		
 		echo "-r -i $mywinid -e 0,${x},${y},${width},${height}"
-		echo "$maximizedHorizontally $maximizedVertically"
 		echo "-i -r $mywinid -e 0,${x},${y},${width},${height}" >> $1
 		
 		if [[ "$maximizedHorizontally" = "1" && "$maximizedVertically" = "1" ]] ; then
@@ -52,60 +51,17 @@ function saveConfiguration () {
 						echo "-i -r ${mywinid} -b remove,maximized_vert" >> $1
 					fi
 		fi
-		
-		# echo "$mywinid $x $y $width $height $maximizedHorizontally $maximizedVertically"
-		# echo "$mywinid $x $y $width $height $maximizedHorizontally $maximizedVertically" >> $1
 	done
 }
 
 function restoreConfiguration () {
 	file=$1
 	echo $1
-	declare -a mywinid
-	declare -a x
-	declare -a y
-	declare -a width
-	declare -a height
 
 	cat "$file" | while read -r line;
 	do
 		echo "wmctrl $line"
 		wmctrl $line
-		# local mywinid=$(echo "$line" | cut -d' ' -f1)
-		# echo $mywinid
-		# if [ "$mywinid" == "0x07e00007" ] ; then
-		# 	local x=$(echo "$line" | cut -d' ' -f2)
-		# 	local y=$(echo "$line" | cut -d' ' -f3)
-		# 	local width=$(echo "$line" | cut -d' ' -f4)
-		# 	local height=$(echo "$line" | cut -d' ' -f5)
-		# 	local maximizedHorizontally=$(echo "$line" | cut -d' ' -f6)
-		# 	local maximizedVertically=$(echo "$line" | cut -d' ' -f7)
-		# 	
-		# 	echo $line
-		# 	echo "$mywinid $x $y $width $height $maximizedHorizontally $maximizedVertically"
-		# 	echo -e `xwininfo -stats -wm -id "$mywinid"`
-		# 
-		# 	if [[ "$maximizedHorizontally" = "1" && "$maximizedVertically" = "1" ]] ; then
-		# 		wmctrl -ir ${mywinid} -b add,maximized_horz,maximized_vert
-		# 	else
-		# 		if [ "$maximizedHorizontally" = "1" ]; then
-		# 			wmctrl -i -r ${mywinid} -b add,maximized_horz
-		# 		else
-		# 			echo "remove maximized_horz"
-		# 			wmctrl -i -r ${mywinid} -b remove,maximized_horz
-		# 			echo "wmctrl -i -r ${mywinid} -b remove,maximized_horz"
-		# 		fi
-		# 		if [ "$maximizedVertically" = "1" ]; then
-		# 			wmctrl -i -r ${mywinid} -b add,maximized_vert
-		# 		else
-		# 			echo "remove maximized_vert"
-		# 			wmctrl -i -r ${mywinid} -b remove,maximized_vert
-		# 			echo "wmctrl -i -r ${mywinid} -b remove,maximized_vert"
-		# 		fi
-		# 		wmctrl -i -r ${mywinid} -e 0,${x},${y},${width},${height}
-		# 		echo "wmctrl -i -r ${mywinid} -e 0,${x},${y},${width},${height}"
-		# 	fi
-		# fi
 	done
 }
 
